5aba8f6fca843a2e09eb69baf7cdd035a4600dc6
Thomas Moerman <thomas.moerman@gmail.com>
2017-04-26 14:14:32 +0200

    set nr of threads in function of data size

--- run_zeisel.py
+++ run_zeisel.py
@@ -4,4 +4,7 @@ TF_path = "/media/tmo/data/work/datasets/TF/mm9_TFs.txt"
 in_path = "/media/tmo/data/work/datasets/zeisel/expression_sara_filtered.txt"
 out_path = "/media/tmo/data/work/datasets/benchmarks/genie3/zeisel/zeisel.filtered.genie3.txt"
 
-run(in_path, TF_path, out_path)
+# nr of threads for Nostromo
+nr_threads = 88
+
+run(in_path, TF_path, out_path, nr_threads=nr_threads)
