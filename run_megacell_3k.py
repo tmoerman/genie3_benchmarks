87811ae9cde0ec6362003353f8205e11b073b3a7
Thomas Moerman <thomas.moerman@gmail.com>
2017-05-22 11:42:23 +0200

    half number of threads

--- run_megacell_3k.py
+++ run_megacell_3k.py
@@ -4,6 +4,6 @@ TF_path  = "/media/tmo/data/work/datasets/TF/mm9_TFs.txt"
 in_path  = "/media/tmo/data/work/datasets/megacell/out/matrix.subsets/3k/nr.rounds.250/from.exclude/subset.3k.from.exclude.txt/part-00000"
 out_path = "/media/tmo/data/work/datasets/benchmarks/genie3/megacell/megacell.3k.genie3.txt"
 
-nr_threads = 88
+nr_threads = 44
 
 run(in_path, TF_path, out_path, nr_threads=nr_threads)
