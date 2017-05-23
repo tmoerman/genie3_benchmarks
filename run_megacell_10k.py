dc7733cd3cc1fcf0e4409cd0a729bfccb52edb34
Thomas Moerman <thomas.moerman@gmail.com>
2017-05-23 21:46:38 +0200

    run megacell with 12 threads

--- run_megacell_10k.py
+++ run_megacell_10k.py
@@ -4,6 +4,6 @@ TF_path  = "/media/tmo/data/work/datasets/TF/mm9_TFs.txt"
 in_path  = "/media/tmo/data/work/datasets/megacell/out/matrix.subsets/10k/nr.rounds.250/from.exclude/subset.10k.from.exclude.txt/part-00000"
 out_path = "/media/tmo/data/work/datasets/benchmarks/genie3/megacell/megacell.10k.genie3.txt"
 
-nr_threads = 88
+nr_threads = 12
 
 run(in_path, TF_path, out_path, nr_threads=nr_threads)
