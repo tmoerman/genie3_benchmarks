c50cabeac89d7bfc1032099a3ec134b6643679a2
Thomas Moerman <thomas.moerman@gmail.com>
2017-05-30 14:48:41 +0200

    dial down nr threads

--- run_MM087_10x.py
+++ run_MM087_10x.py
@@ -4,6 +4,6 @@ TF_path  = "/media/tmo/data1/work/datasets/katina/MM087_TFs_10x.lst"
 in_path  = "/media/tmo/data1/work/datasets/katina/MM087_10x.tsv"
 out_path = "/media/tmo/data1/work/datasets/katina/out/MM087_10x.out.tsv"
 
-nr_threads = 88
+nr_threads = 44
 
 run(in_path, TF_path, out_path, nr_threads=nr_threads)
