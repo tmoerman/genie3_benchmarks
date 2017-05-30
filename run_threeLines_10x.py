fea0dc31be9a7ecd1720bd39e360cee7bb388fec
Thomas Moerman <thomas.moerman@gmail.com>
2017-05-30 23:00:21 +0200

    threeLines 10x (15944 cells, 13729 genes)

---
+++ run_threeLines_10x.py
@@ -0,0 +1,9 @@
+from sc_benchmark import run
+
+TF_path  = "/media/tmo/data1/work/datasets/katina/threeLines_TFs_10x.lst"
+in_path  = "/media/tmo/data1/work/datasets/katina/threeLines_10x.tsv"
+out_path = "/media/tmo/data1/work/datasets/katina/out/threeLines_10x.out.tsv"
+
+nr_threads = 16
+
+run(in_path, TF_path, out_path, nr_threads=nr_threads)
