c3b6e8b0835ffeafb30cc3eb896af367345716d5
Thomas Moerman <thomas.moerman@gmail.com>
2017-05-30 14:41:50 +0200

    meh

---
+++ run_MM087_10x.py
@@ -0,0 +1,9 @@
+from sc_benchmark import run
+
+TF_path  = "/media/tmo/data1/work/datasets/katina/MM087_TFs_10x.lst"
+in_path  = "/media/tmo/data1/work/datasets/katina/MM087_10x.tsv"
+out_path = "/media/tmo/data1/work/datasets/katina/out/MM087_10x.out.tsv"
+
+nr_threads = 88
+
+run(in_path, TF_path, out_path, nr_threads=nr_threads)
